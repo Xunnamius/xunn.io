c5e76bc1092a4619730a24f3d3c513346f329841
Xunnamius (Anarchy) <Xunnamius@users.noreply.github.com>
2016-04-05 06:18:26 -0500

    updated index.php

--- public/index.php
+++ public/index.php
@@ -1,1 +1,12 @@
-<?= 'Hello, world!' ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Xunn Wuz Here</title>
+    </head>
+    <body>
+<?php
+        echo '<h1>Hello, world!</h1>';
+        phpinfo();
+?>
+    </body>
+</html>
