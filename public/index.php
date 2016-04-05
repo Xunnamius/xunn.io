7657bcfe5967a1df70e1ca2a186e95f196b79c0f
Xunnamius (Anarchy) <Xunnamius@users.noreply.github.com>
2016-04-05 08:17:37 -0500

    preparing for javascript

--- public/index.php
+++ public/index.php
@@ -23,7 +23,7 @@
                 width: 100%;
                 height: 100%;
                 background-color: black;
-                opacity: 0.4;
+                opacity: 0.8;
             }
 
             #logo {
