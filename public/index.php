dc944c507f9364d3eacc77724e39e65da2891b60
Xunnamius (Dynasty) <Xunnamius@users.noreply.github.com>
2016-04-06 11:17:29 -0500

    removed ergodark logo

--- public/index.php
+++ public/index.php
@@ -22,15 +22,24 @@
                 width: 100%;
                 height: 100%;
                 background-color: black;
-                opacity: 0.8;
+                opacity: 0.7;
                 position: absolute;
                 top: 0;
                 left: 0;
+            }
+
+            h1 {
+                text-align: center;
+                margin-top: 50vh;
+                color: #909090;
+                position: relative;
+                font-family: sans-serif;
+                font-variant: small-caps;
             }
         </style>
     </head>
     <body>
         <div id="cover1"></div>
-        <h1>What you seek is coming soon...</h1>
+        <h1>what you seek is coming soon</h1>
     </body>
 </html>
